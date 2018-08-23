ef0fb41031036d746f3dceb204d592d5b915fdcf
pabloamco <pablol@amcoonline.net>
2018-08-22 20:53:47 -0500

    Rendering html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	def toy_app
+		render html: "No world Today I won't say Hello!... lol just hate u"
+	end
 end
